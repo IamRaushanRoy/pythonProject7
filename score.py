2a0f610652e69d3118ff6533d6d9501fa73f1b33
Raushan Kumar <77504635+IamRaushanRoy@users.noreply.github.com>
2021-01-28 04:50:46 +0530

    Initial commit

---
+++ score.py
@@ -0,0 +1,35 @@
+class students:
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+        self.marks = []
+        students.count = students.count + 1
+
+    def enterMarks(self):
+        for i in range(6):
+            m = int(input("Enter the marks of %s in %d subject: " % (self.name, i + 1)))
+            self.marks.append(m)
+
+    def display(self):
+        print(self.name, "got ", self.marks)
+
+
+name = input("Enter the name of Student:")
+s1 = students(name)
+s1.enterMarks()
+s1.display()
+print("")
+name = input("Enter the name of Student:")
+s2 = students(name)
+s2.enterMarks()
+s2.display()
+name = input("Enter the name of Student:")
+s3 = students(name)
+s3.enterMarks()
+s3.display()
+name = input("Enter the name of Student:")
+s4 = students(name)
+s4.enterMarks()
+s4.display()
+
